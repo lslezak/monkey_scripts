155c7c11c9ef0bb4a486c9697c99b8038a92992e
Ladislav Slezák <lslezak@suse.cz>
2019-03-06 15:26:04 +0100

    bugzilla_trello_integration.user.js - better handle missing avatar icon

--- bugzilla_trello_integration.user.js
+++ bugzilla_trello_integration.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bugzilla Trello integration
 // @namespace    https://blog.ladslezak.cz/
-// @version      0.1.3
+// @version      0.1.4
 // @description  Integrate Bugzilla with Trello
 // @author       Ladislav Slezák
 // @match        https://bugzilla.suse.com/show_bug.cgi*
@@ -278,6 +278,8 @@
         }
 
         var avatars = card.members.map(function(m) {
+            if (m.avatarHash === null) return "(" + escapeHtml(m.fullName) + ") ";
+
             return "<img class=\"avatar_icon\" src=\"https://trello-avatars.s3.amazonaws.com/" +
               escapeHtml(m.avatarHash) + "/30.png\" title=\"" + escapeHtml(m.fullName) + "\">";
         });
